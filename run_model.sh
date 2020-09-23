2999eff8f3d5576d24cb96af15fc19b0f1f1576d
weilingwei96 <67113788+weilingwei96@users.noreply.github.com>
2020-09-23 21:40:43 +0800

    Update run_model.sh

--- run_model.sh
+++ run_model.sh
@@ -4,15 +4,17 @@
 ## transfer tfrecorder
 
 # input path
-unsup_train_input=/root/wlw/UDA_pytorch/data/proc_data/unsup/bt-0.9/0/tf_examples.tfrecord.0.0
-sup_train_input=/root/wlw/UDA_pytorch/data/proc_data/train_20/tf_examples.tfrecord.0.0
-test_input=/root/wlw/UDA_pytorch/data/proc_data/dev/tf_examples.tfrecord.0.0
+unsup_train_input=./data/proc_data/unsup/bt-0.9/0/tf_examples.tfrecord.0.0
+sup_train_input=./data/proc_data/train_20/tf_examples.tfrecord.0.0
+test_input=./data/proc_data/dev/tf_examples.tfrecord.0.0
 
 # output path
-unsup_train_output=/root/wlw/UDA_pytorch/demo/imdb_unsup_train.txt
-sup_train_output=/root/wlw/UDA_pytorch/demo/imdb_sup_train.txt
-test_output=/root/wlw/UDA_pytorch/demo/imdb_sup_test.txt
+unsup_train_output=./data/demo/imdb_unsup_train.txt
+sup_train_output=./data/demo/imdb_sup_train.txt
+test_output=./data/demo/imdb_sup_test.txt
 
+
+mkdir data/demo
 echo 'transfer...'
 echo $unsup_train_input+'>'+$unsup_train_output
 echo $sup_train_input+'>'+$sup_train_output
@@ -26,11 +28,9 @@ python -u tfRecorder_to_txt.py \
     --sup_test_input_data=${test_input} \
     --sup_test_output_data=${test_output}
 
-echo '\n\n\n\n\n'
 echo 'UDA - pytorch running...'
 cat config/demo_uda.json
-# change uda_config path 
 python -u main.py \
       --uda_config='config/demo_uda.json' \
-      --bert_base_config='config/chinese_bert_base.json'
+      --bert_base_config='config/bert_base.json'
 
